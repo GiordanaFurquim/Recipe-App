bc3f609649474932a0a39eb2afe98ca92a568467
Giordana Furquim <giordanafurquim@gmail.com>
2019-11-07 12:10:19 +0100

    sanity commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Form from "./components/Form";
 const API_KEY = "b2426c698473d5b2f9ec23223aef62f1";
 
 class App extends Component {
+  state = {
+    recipes: []
+  }
+
   getRecipe = async (e) => {
     e.preventDefault();
 
@@ -13,9 +17,10 @@ class App extends Component {
     const api_call = await fetch(`https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`);
     
     const data = await api_call.json();
-    console.log(data);
     
+    this.setState({ recipes: data.recipes });    
   }
+
   render() {
     return (
       <div className="App">
@@ -23,6 +28,9 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        { this.state.recipes.map((recipe) => {
+          return <p key={recipe.recipe_id}>{recipe.title}</p>
+        }) } 
       </div>
     );
   }
